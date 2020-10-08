930494c5ef38661ae4d007d8389e8c8cb6b50234
GabeLlanos <gabriel.llanos@patelhs.org>
2020-10-08 14:42:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+
+ 
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+     boolean CorrectNumber = true;
+     if (CorrectNumber)
+     {
+         System.out.println("You Win! ");
+     }
+     else
+     {
+         System.out.println("Try again text time ");
+     }
+     System.out.println("after conditional");
+   }
+}
